2c4329c55d9733797fb9caacf5a796e8e2d1928d
gamer <gamer@PC-FELIPE76>
2024-05-05 19:48:12 -0300

    Adicionar arquivos de projeto.

---
+++ SchoolAverage/Program.cs
@@ -0,0 +1,13 @@
+﻿using SchoolAverage;
+
+Student s = new Student();
+Console.Write("Enter name: ");
+s.name = Console.ReadLine();
+
+Console.Write("Enter grade1: ");
+s.grade1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter grade2: ");
+s.grade2 = Convert.ToInt32(Console.ReadLine());
+
+s.message();
---
+++ SchoolAverage/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAverage
+{
+    internal class Student
+    {
+        public string? name;
+        public double grade1, grade2;
+
+        //Average
+        public double average()
+        {
+            return (grade1 + grade2)/2;
+        }
+        //Situation
+        public string situation(double average)
+        {
+            return average >= 7 ? "Approved" : "Disapproved";
+        }
+        //Message
+        public void message()
+        {
+            double getAverage = average();
+            string getSituation = situation(getAverage);
+            Console.WriteLine(name + " it is " +getSituation+" with average of "+getAverage);
+        }
+    }
+}
